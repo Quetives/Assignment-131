67ec5bbcf0993ee320b065eb48b9a217f5470386
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-03 21:03:32 -0800

    Add project files.

---
+++ Assignment 131/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Richard";
+            string hobby = "coding";
+            string location = "home";
+
+            Console.WriteLine(name + " likes to do " + hobby + " from " + location + "!");
+
+            Console.WriteLine(name.ToUpper());
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("This is a paragraph of text.");
+            sb.Append("\nIt is made one sentence at a time.");
+            sb.Append("\nI am not sure what it's about.");
+            sb.Append("\nRegardless, here it is!");
+
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+
+        }
+    }
+}
